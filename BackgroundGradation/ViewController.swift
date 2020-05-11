1033f6ee98bad9e5a2a20123c25fd91206f2d71a
Isseymiyamoto <ants.can.walk@gmail.com>
2020-05-11 22:31:48 +0900

    backgroundにグラデーションカラーを配置する事に成功

--- BackgroundGradation/ViewController.swift
+++ BackgroundGradation/ViewController.swift
@@ -9,10 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let ChangeColorButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Change Color", for: .normal)
+        button.setTitleColor(.systemPink, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        button.backgroundColor = .systemGroupedBackground
+        button.addTarget(self, action: #selector(handleChangeBackgroundColor), for: .touchUpInside)
+        button.layer.cornerRadius = 30
+        return button
+    }()
+    
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configure()
+        configureNavigationBar(color1: UIColor.red.cgColor, color2: UIColor.blue.cgColor)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.title = "Gradient Color"
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.barStyle = .default
+        navigationController?.navigationBar.isHidden = false
+    }
+    
+    // MARK: - Helpers
+    
+    func configure(){
+        view.backgroundColor = .white
+        
+        view.addSubview(ChangeColorButton)
+        ChangeColorButton.center(inView: view)
+        ChangeColorButton.setDimensions(width: view.frame.width - 64, height: 60)
+    }
+    
+    func configureNavigationBar(color1: CGColor, color2: CGColor ){
+        if let navigationBar = self.navigationController?.navigationBar {
+            let gradient = CAGradientLayer()
+            var bounds = navigationBar.bounds
+            bounds.size.height += self.additionalSafeAreaInsets.top
+            gradient.frame = bounds
+            gradient.colors = [color1, color2]
+            gradient.startPoint = CGPoint(x: 0, y: 0)
+            gradient.endPoint = CGPoint(x: 1, y: 0)
+            
+            if let image = getImageFrom(gradientLayer: gradient) {
+                navigationBar.setBackgroundImage(image, for: UIBarMetrics.default)
+            }
+        }
+    }
+    
+    func getImageFrom(gradientLayer:CAGradientLayer) -> UIImage? {
+        var gradientImage:UIImage?
+        UIGraphicsBeginImageContext(gradientLayer.frame.size)
+        if let context = UIGraphicsGetCurrentContext() {
+            gradientLayer.render(in: context)
+            gradientImage = UIGraphicsGetImageFromCurrentImageContext()?.resizableImage(withCapInsets: UIEdgeInsets.zero, resizingMode: .stretch)
+        }
+        UIGraphicsEndImageContext()
+        return gradientImage
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleChangeBackgroundColor(){
+        // navigationBarの色を変える処理をかく
+        
     }
 
 
